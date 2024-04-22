9e96bd5af62775a70fc745565fe38fe0a1fa0ad8
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2024-04-22 14:28:17 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello Word");
-})
+app.get("/health", (req, res) => {
+  console.log("I am in health api");
+  res.json({
+    service: "Backend Job Listing API Server",
+    status: "active",
+    time: new Date(),
+  });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("port is running on ", PORT);
